e9e5270af25dea9d8ce3bf21460c421948b61d0f
micou1830 <micou1830@hotmail.fr>
2019-03-11 15:58:10 +0100

    Init project

---
+++ src/main/java/com/hoc/motobox/MotoboxApplication.java
@@ -0,0 +1,13 @@
+package com.hoc.motobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoc/motobox/MotoboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hoc.motobox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MotoboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
