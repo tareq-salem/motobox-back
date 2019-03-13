c8c6dd14beb7e7585b3a31d5c0849e4ee9d5e4d7
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2019-03-13 15:57:34 +0100

    Tareq/next work (#15)
    * Add Postman settings file
    
    * fix my properties
    
    * histoire de pouvoir puller

--- src/main/java/com/hoc/motobox/exception/FileStorageException.java
+++ src/main/java/com/hoc/motobox/exception/FileStorageException.java
@@ -9,9 +9,7 @@ package com.hoc.motobox.exception;
  */
 public class FileStorageException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public FileStorageException(String message) {
