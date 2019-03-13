f4dd830bb1c14bdf63acdc7b8e820a248276020a
Djahil <38948430+Djahil@users.noreply.github.com>
2019-03-13 14:34:31 +0100

    Hafidhou/Fix bugs (#12)

--- src/main/java/com/hoc/motobox/MotoboxApplication.java
+++ src/main/java/com/hoc/motobox/MotoboxApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import com.hoc.motobox.property.FileStorageProperties;
 
 @SpringBootApplication
-
 @EnableConfigurationProperties({ FileStorageProperties.class })
 public class MotoboxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MotoboxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MotoboxApplication.class, args);
+    }
 
 }
