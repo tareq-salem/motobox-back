8f5278074148d3c2c607d96a8440bb5595d94b78
Vero-Morane Lafaverges <veroxy@users.noreply.github.com>
2019-03-14 16:57:10 +0100

    Pitou (#24)
    * paramtres bdd mysql driver -- jbdc.mysql -- hibernate core && entitymanger
    
    * Utils > generic elements for extends Entities && Contoller,  for implement sUPERsERVICE
    
    * FIXED SuperEnitty ok: preppersit & preupdate -- id is autogenerated from this generic class.  TOFIXED persistence updateAt tot test
    
    * Delete UserServiceImpl FIXED
    
    * Delete Cart --  Replaced by ManyToMany Relation between User & Ad

--- src/main/java/com/hoc/motobox/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hoc.motobox.repository;
-
-import com.hoc.motobox.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
