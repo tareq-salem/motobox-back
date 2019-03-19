6cd59235c31acc4efca8edef6d3452a739986edf
Vero-Morane Lafaverges <veroxy@users.noreply.github.com>
2019-03-19 11:54:34 +0100

    Search piece (#40)
    * Add Postman settings file
    
    * fix my properties
    
    * AdController 'travail en cours'
    
    * Mettez vos repo à jour car j'ai récupérer des truc que j'avais déjà modifier et push - !
    
    * ajouter un ad
    
    * error from user -- get Role
    
    * Fixed -- deleting constructor in category && syubcategory
    
    * MAJ app properties,  no creat&drop just update db for update datas
    
    * category with none subclass are available to be resquest with discriminator -- four last elments in the array JSON (Ad entity)

--- src/main/java/com/hoc/motobox/entity/category/BobineAllumage.java
+++ src/main/java/com/hoc/motobox/entity/category/BobineAllumage.java
@@ -1,6 +1,7 @@
 package com.hoc.motobox.entity.category;
 import javax.persistence.Entity;
 
+import com.hoc.motobox.entity.family.PartieElectrique;
 @Entity
 public class BobineAllumage extends PartieElectrique {
 }
--- src/main/java/com/hoc/motobox/entity/category/RegulateurDeTension.java
+++ src/main/java/com/hoc/motobox/entity/category/RegulateurDeTension.java
@@ -1,6 +1,7 @@
 package com.hoc.motobox.entity.category;
 import javax.persistence.Entity;
 
-@Entity
+import com.hoc.motobox.entity.family.PartieElectrique;
 public class RegulateurDeTension extends PartieElectrique {
+@Entity
 }
